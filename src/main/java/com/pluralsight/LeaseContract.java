c7ed50bfcd4f7b09f366a2de20c7a077bcb70777
Andi <alexandra.e.burke@gmail.com>
2023-11-13 15:51:53 -0500

    finished the Lease Contract Class

--- src/main/java/com/pluralsight/LeaseContract.java
+++ src/main/java/com/pluralsight/LeaseContract.java
@@ -1,8 +1,11 @@
 package com.pluralsight;
 
+import static com.pluralsight.Vehicle.getPrice;
+
 public class LeaseContract extends Contract{
     protected double expectedEndingValue = 0.5;
-    protected double leaseFee = 0.07;
+    protected double leaseFee = 0.07 ;
+
 
     public LeaseContract(String date, String customerName, String customerEmail, String vehicleSold, double expectedEndingValue, double leaseFee) {
         super(date, customerName, customerEmail, vehicleSold);
@@ -11,12 +14,29 @@ public class LeaseContract extends Contract{
     }
 
     public double getExpectedEndingValue() {
+        expectedEndingValue = getPrice() * expectedEndingValue;
         return expectedEndingValue;
     }
 
     public double getLeaseFee() {
         return leaseFee;
     }
+
+    @Override
+    public double getTotalPrice() {
+        double totalPrice = getPrice() + expectedEndingValue + leaseFee;
+        return totalPrice;
+    }
+
+    @Override
+    public double getMonthlyPayment() {
+        double monthly = getTotalPrice() /36;
+        double interest = (getTotalPrice() /4.0) / 100;
+        double monthlyLeasePayment = monthly + interest;
+        return monthlyLeasePayment;
+
+    }
+
     //@Override
     //        public double getValue() {
     //            return (originalCost / squareFoot) + (.25 * lotSize);
